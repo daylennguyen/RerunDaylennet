0356f540fde42a9d7be8d252090362fa68b7e8de
daylen <daylennguyen@gmail.com>
2019-03-01 20:40:04 -0800

    successfully created intro template, needs style

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,84 +1,10 @@
 ---
 templateKey: "index-page"
 title: "maintitle"
-image: /img/home-jumbotron.jpg
-heading: Heading
-subheading: Subheading
-description: >-
-  home.description
-mainpitch:
-  title: MainPitch.title
-  description: >
-    mainpitch.description
+banner: /img/home-jumbotron.jpg
+termtitle: "Terminal"
+termtext: "I'm a term"
 intro:
-  blurbs:
-    - image: /img/coffee.png
-      text: >
-        --intro.blurbs.text.1
-    - image: /img/coffee-gear.png
-      text: >
-        --intro.blurbs.text.2
-    - image: /img/tutorials.png
-      text: >
-        --intro.blurbs.text.3
-    - image: /img/meeting-space.png
-      text: >
-        --intro.blurbs.text.4
-  heading: What we offer
-  description: >
-    home.intro.description
-main:
-  heading: Great coffee with no compromises
-  description: >
-    We hold our coffee to the highest standards from the shrub to the cup.
-    That’s why we’re meticulous and transparent about each step of the coffee’s
-    journey. We personally visit each farm to make sure the conditions are
-    optimal for the plants, farmers and the local environment.
-  image1:
-    alt: A close-up of a paper filter filled with ground coffee
-    image: /img/products-grid3.jpg
-  image2:
-    alt: A green cup of a coffee on a wooden table
-    image: /img/products-grid2.jpg
-  image3:
-    alt: Coffee beans
-    image: /img/products-grid1.jpg
-testimonials:
-  - author: Elisabeth Kaurismäki
-    quote: >-
-      The first time I tried Kaldi’s coffee, I couldn’t even believe that was
-      the same thing I’ve been drinking every morning.
-  - author: Philipp Trommler
-    quote: >-
-      Kaldi is the place to go if you want the best quality coffee. I love their
-      stance on empowering farmers and transparency.
-full_image: /img/products-full-width.jpg
-pricing:
-  heading: Monthly subscriptions
-  description: >-
-    We make it easy to make great coffee a part of your life. Choose one of our
-    monthly subscription plans to receive great coffee at your doorstep each
-    month. Contact us about more details and payment info.
-  plans:
-    - description: Perfect for the drinker who likes to enjoy 1-2 cups per day.
-      items:
-        - 3 lbs of coffee per month
-        - Green or roasted beans"
-        - One or two varieties of beans"
-      plan: Small
-      price: "50"
-    - description: "Great for avid drinkers, java-loving couples and bigger crowds"
-      items:
-        - 6 lbs of coffee per month
-        - Green or roasted beans
-        - Up to 4 different varieties of beans
-      plan: Big
-      price: "80"
-    - description: Want a few tiny batches from different varieties? Try our custom plan
-      items:
-        - Whatever you need
-        - Green or roasted beans
-        - Unlimited varieties
-      plan: Custom
-      price: "??"
+  heading: "main.intro.heading"
+  body: main.intro.body
 ---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,50 +3,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/Layout'
-import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
 // Query data parsed from MD as input
 export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-  main
+  banner,
+  termTitle,
+  termText,
+  introHeading,
+  introBody
 }) => (
-  <div>
-    {title}
-    {subheading}
-    <h1 className='title'>{mainpitch.title}</h1>
-    <h3 className='subtitle'>{mainpitch.description}</h3>
-    <h3 className='has-text-weight-semibold is-size-2'>{heading}</h3>
-    <p>{description}</p>
-    <Features gridItems={intro.blurbs} />
-    <Link className='btn' to='/products'>
-      See all products
+    <div>
+      <Img fluid={banner.childImageSharp.fluid} />
+      <p>{termTitle}</p>
+      <p>{termText}</p>
+      <p>{introHeading}</p>
+      <p>{introBody}</p>
+      <Link className='btn' to='/products'>
+        See all products
     </Link>
-    <BlogRoll />
-    <Link className='btn' to='/blog'>
-      Read more
+      <BlogRoll />
+      <Link className='btn' to='/blog'>
+        Read more
     </Link>
-  </div>
-)
+    </div>
+  )
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array
-  })
+  banner: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  termTitle: PropTypes.string,
+  termText: PropTypes.string,
+  introHeading: PropTypes.string,
+  introBody: PropTypes.string,
 }
 
 const IndexPage = ({ data }) => {
@@ -55,13 +46,11 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
+        banner={frontmatter.banner}
+        termTitle={frontmatter.termtitle}
+        termText={frontmatter.termtext}
+        introHeading={frontmatter.intro.heading}
+        introBody={frontmatter.intro.body}
       />
     </Layout>
   )
@@ -81,34 +70,18 @@ export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
-        title
-        image {
+        banner {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
               ...GatsbyImageSharpFluid
             }
           }
         }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
+        termtitle
+        termtext
         intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
           heading
-          description
+          body
         }
       }
     }
