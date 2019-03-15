53afb7874962c85a220f912682467c014697c1f7
daylen <daylennguyen@gmail.com>
2019-03-14 21:42:04 -0700

    added avatar and easter egg. finish console

--- src/templates/TermCollectionsNav.js
+++ src/templates/TermCollectionsNav.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-const BtnsWithTxt = ({ txt, ...restProps }) => <Link {...restProps}>{txt}</Link>
+const BtnsWithTxt = ({ txt, ...restProps }) => (
+	<Link {...restProps}>{txt}</Link>
+);
 
 export default () => (
-  <div className=" buttons is-grouped has-addons are-medium">
-    <BtnsWithTxt to="/blog" className="button" txt="Programming" />
-    <BtnsWithTxt to="/blog" className="button" txt="Blog" />
-    <BtnsWithTxt to="/blog" className="button" txt="Photography" />
-  </div>
-)
+	<div
+		className="buttons is-grouped has-addons are-medium"
+		style={{ margin: "10px" }}
+	>
+		<BtnsWithTxt to="/blog" className="button" txt="Programming" />
+		<BtnsWithTxt to="/blog" className="button" txt="Blog" />
+		<BtnsWithTxt to="/blog" className="button" txt="Photography" />
+	</div>
+);
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,112 +1,106 @@
 // Landing Page
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import HomeBanner from './banner.svg'
-import Layout from '../components/Layout'
-import BlogRoll from '../components/BlogRoll'
-import Terminal from '../components/Terminal'
-import { Social } from './Social'
-import TermCollectionsNav from './TermCollectionsNav'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import HomeBanner from "./banner.svg";
+import Layout from "../components/Layout";
+import BlogRoll from "../components/BlogRoll";
+import Terminal from "../components/Terminal";
+import Avatar from "../components/Avatar";
+import { Social } from "./Social";
+import TermCollectionsNav from "./TermCollectionsNav";
 
-{
-  /* <img src={`${HomeBanner}`} className="banner" alt="daylen.net website portfolio banner" /> */
-}
+/* <img src={`${HomeBanner}`} className="banner" alt="daylen.net website portfolio banner" /> */
+
 export const IndexPageTemplate = ({
-  introHeading,
-  introBody,
-  termTitle,
-  init,
-  replaceList,
+	introHeading,
+	introBody,
+	termTitle,
+	init,
+	replaceList
 }) => {
-  const tBanner = (
-    <div className="avatar avatar-fit">
-      <div className="color" />
-      <div className="eyes"> </div>
-      <div className="mouth"> </div>
-    </div>
-  )
-  return (
-    <div>
-      <div className="flex-center land-wrapper head">
-        <Terminal
-          name={termTitle}
-          banner={tBanner}
-          init={init}
-          replaceList={replaceList}
-        >
-          <TermCollectionsNav />
-        </Terminal>
-        <Social />
-      </div>
+	const tBanner = <Avatar />;
+	return (
+		<div>
+			<div className="flex-center land-wrapper head">
+				<Terminal
+					name={termTitle}
+					banner={tBanner}
+					init={init}
+					replaceList={replaceList}
+				>
+					<TermCollectionsNav />
+				</Terminal>
+				<Social />
+			</div>
 
-      <section className="content margin-content">
-        <div className="container">
-          <h1>{termTitle}</h1>
-          <h1>{init}</h1>
-          <h1>{replaceList}</h1>
-          <h1>{introHeading}</h1>
-          <h1>{introBody}</h1>
-          <BlogRoll />
-        </div>
-      </section>
-    </div>
-  )
-}
+			<section className="content margin-content">
+				<div className="container">
+					<h1>{termTitle}</h1>
+					<h1>{init}</h1>
+					<h1>{replaceList}</h1>
+					<h1>{introHeading}</h1>
+					<h1>{introBody}</h1>
+					<BlogRoll />
+				</div>
+			</section>
+		</div>
+	);
+};
 
 // Type Validation
 IndexPageTemplate.propTypes = {
-  banner: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  termTitle: PropTypes.string,
-  termText: PropTypes.string,
-  introHeading: PropTypes.string,
-  introBody: PropTypes.string,
-  init: PropTypes.string,
-  replaceList: PropTypes.array,
-}
+	banner: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+	termTitle: PropTypes.string,
+	termText: PropTypes.string,
+	introHeading: PropTypes.string,
+	introBody: PropTypes.string,
+	init: PropTypes.string,
+	replaceList: PropTypes.array
+};
 
 // Retrieve GQL data, insert into layout_component
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
-  return (
-    <Layout>
-      <IndexPageTemplate
-        banner={frontmatter.banner}
-        introHeading={frontmatter.intro.heading}
-        introBody={frontmatter.intro.body}
-        init={frontmatter.terminal.termInit}
-        termTitle={frontmatter.terminal.termtitle}
-        replaceList={frontmatter.terminal.replaceList}
-      />
-    </Layout>
-  )
-}
+	const { frontmatter } = data.markdownRemark;
+	return (
+		<Layout>
+			<IndexPageTemplate
+				banner={frontmatter.banner}
+				introHeading={frontmatter.intro.heading}
+				introBody={frontmatter.intro.body}
+				init={frontmatter.terminal.termInit}
+				termTitle={frontmatter.terminal.termtitle}
+				replaceList={frontmatter.terminal.replaceList}
+			/>
+		</Layout>
+	);
+};
 
 IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+	data: PropTypes.shape({
+		markdownRemark: PropTypes.shape({
+			frontmatter: PropTypes.object
+		})
+	})
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        terminal {
-          termtitle
-          termInit
-          replaceList
-        }
-        intro {
-          heading
-          body
-        }
-      }
-    }
-  }
-`
+	query IndexPageTemplate {
+		markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+			frontmatter {
+				terminal {
+					termtitle
+					termInit
+					replaceList
+				}
+				intro {
+					heading
+					body
+				}
+			}
+		}
+	}
+`;
